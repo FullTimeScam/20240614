95f93cc4077ab29672b31c2ca7192508369eca4c
FullTimeScam <418.gingin@gmail.com>
2024-06-14 16:46:59 +0900

    버튼 클릭 시 해당 페이지로 스크롤

---
+++ scroll-practice/src/App.tsx
@@ -0,0 +1,11 @@
+import { Flex } from "@chakra-ui/react";
+import { FC } from "react";
+import A from "./components/A";
+import B from "./components/B";
+import C from "./components/C";
+
+const App: FC = () => {
+  return <Flex flexDir={"column"}><A/><B/><C/></Flex>;
+};
+
+export default App;
